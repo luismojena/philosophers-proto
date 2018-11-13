29b4eea4f16d7ddce931b3c2a7ea04e8f9e54067
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-11-13 22:47:01 +0000

    Add some data types Still have to make a reconciliation with all data types in SQLAlchemy

--- types.py
+++ types.py
@@ -2,6 +2,18 @@
 Data types management for .proto files compilation
 """
 
+import enum
 
-class DataType:
-    pass
+
+# todo: finish datatype mappings
+class DataType(enum.Enum):
+    int = 'int'
+    float = 'float'
+    rpc = 'rpc'
+    message = 'message'
+    double = 'double'
+    int16 = 'int16'
+    int32 = 'int32'
+    int64 = 'int64'
+    long = 'long'
+    repeated = 'repeated'
